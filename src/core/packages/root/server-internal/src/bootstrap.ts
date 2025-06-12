e2110ff138e007231af87df68a740191623ea459
Elena Shostak <elena.shostak@elastic.co>
2025-06-12 16:49:11 +0200

    Added fs proxy setup

--- src/core/packages/root/server-internal/src/bootstrap.ts
+++ src/core/packages/root/server-internal/src/bootstrap.ts
@@ -12,6 +12,7 @@ import { getPackages } from '@kbn/repo-packages';
 import type { CliArgs } from '@kbn/config';
 import { Env, RawConfigService } from '@kbn/config';
 import { CriticalError } from '@kbn/core-base-server-internal';
+import { fsEventBus } from '@kbn/security-hardening';
 import { Root } from './root';
 import { MIGRATION_EXCEPTION_CODE } from './constants';
 
@@ -52,6 +53,18 @@ export async function bootstrap({ configs, cliArgs, applyConfigOverrides }: Boot
   const root = new Root(rawConfigService, env, onRootShutdown);
   const cliLogger = root.logger.get('cli');
   const rootLogger = root.logger.get('root');
+
+  rawConfigService.getConfig$().subscribe((config) => {
+    const loggerFilePath = Object.values(config.logging.appenders)
+      // @ts-expect-error
+      .filter((appender) => appender.type === 'file')
+      // @ts-expect-error
+      .map((appender) => appender.fileName);
+
+    if (loggerFilePath.length) {
+      fsEventBus.emit('kbn_config_changed', { loggerFilePath });
+    }
+  });
 
   rootLogger.info('Kibana is starting');
 
